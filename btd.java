3a7bd2d4fde2c18dcdff828f5a4e7cb921bb661a
kavirajthakur <thakurkavirajofficial@gmail.com>
2022-11-19 20:45:51 +0530

    changes

---
+++ btd.java
@@ -0,0 +1,7 @@
+public class btd {
+    public static void main(String args[]){
+        String binaryNumber = "101";
+        System.out.println(Integer.parseInt(binaryNumber,2));
+    }
+}
+  jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
